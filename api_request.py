54647fc9fd4707969dadb0c562cb747186b0d1ce
ahmedsaleh17 <ahmedsalehsh@gmail.com>
2026-04-13 15:03:14 +0200

    add python script to  request weatherdata from WeatherStack api

--- api_request.py
+++ api_request.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import os
 from dotenv import load_dotenv
@@ -5,10 +6,43 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Get API key from environment variable
-api_key = os.getenv("WEATHERSTACK_API_KEY")
 
-if not api_key:
-    raise ValueError("WEATHERSTACK_API_KEY not found. Please set it in your .env file.")
+# make a request to api and return content
+def make_request(api_url):
 
-api_url = f"http://api.weatherstack.com/current?access_key={api_key}&query=New York"
+    print("Ingesting weather data from WeatherStack API...")
+    try:
+        # send a HTTP request and receive a response
+
+        response = requests.get(api_url)
+        response.raise_for_status()
+        print("API response received successfully")
+        # weather json data
+        data = response.json()
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+
+
+if __name__ == "__main__":
+    # Get API key from environment variable
+    api_key = os.getenv("WEATHERSTACK_API_KEY")
+
+    if not api_key:
+        raise ValueError(
+            "WEATHERSTACK_API_KEY not found. Please set it in your .env file."
+        )
+
+    api_url = f"http://api.weatherstack.com/current?access_key={api_key}&query=New York"
+
+    # get weather data
+
+    weather_data = make_request(api_url)
+
+    print(weather_data)
+
+    file_name = "weather_data_sample.json"
+
+    with open(file_name, "w", encoding="utf-8") as file:
+        json.dump(weather_data, file, indent=4)
